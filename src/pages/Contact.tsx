75ad36ff231256d42f83ae4011a1ef2a997c69cb
shuhei.tachikawa <shuhei.tusx@gmail.com>
2024-04-26 14:08:52 +0900

    fix

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,54 +1,47 @@
-import React, { ChangeEventHandler, FormEventHandler, useState } from "react";
+import React, { useState } from "react";
+
+
 
 function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
 
-  const [nameErrorMessage, setNameErrorMessage] = useState("");
-  const [emailErrorMessage, setEmailErrorMessage] = useState("");
-  const [messageErrorMessage, setMessageErrorMessage] = useState("");
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
 
-  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    setNameErrorMessage("");
-    setEmailErrorMessage("");
-    setMessageErrorMessage("");
+  const [nameErrorMessage, setNameErrorMessage] = useState("")
+  const [emailErrorMessage, setEmailErrorMessage] = useState("")
+  const [messageErrorMessage, setMessageErrorMessage] = useState("")
+
+  const onSubmit = (e:React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setNameErrorMessage("")
+    setEmailErrorMessage("")
+    setMessageErrorMessage("")
 
     const formData = {
       name,
       email,
-      message,
-    };
+      message
+    }
 
-    const emptyName = name === "";
-    const insufficientNameLength = message.length > 30;
-    const emptyEmail = email === "";
-    const emptyMessage = message === "";
-    const insufficientMessageLength = message.length > 500;
-    const regex =
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@+[a-zA-Z0-9-]+\.+[a-zA-Z0-9-]+$/;
-    if (emptyName) setNameErrorMessage("お名前は必須です。");
-    if (insufficientNameLength)
-      setNameErrorMessage("30文字以内で入力してください。");
-    if (emptyEmail) setEmailErrorMessage("メールアドレスは必須です。");
-    if (!regex.test(email))
-      setEmailErrorMessage("emailの形式で入力してください。");
-    if (emptyMessage) setMessageErrorMessage("本文は必須です。");
-    if (insufficientMessageLength)
-      setMessageErrorMessage("本文は500文字以内で入力してください。");
+    const emptyName = name === ""
+    const insufficientNameLength = message.length > 30
+    const emptyEmail = email === ""
+    const emptyMessage = message === ""
+    const insufficientMessageLength = message.length > 500
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@+[a-zA-Z0-9-]+\.+[a-zA-Z0-9-]+$/
+    if(emptyName) setNameErrorMessage("お名前は必須です。")
+    if(insufficientNameLength) setNameErrorMessage("30文字以内で入力してください。")
+    if(emptyEmail) setEmailErrorMessage("メールアドレスは必須です。")
+    if (!regex.test(email)) setEmailErrorMessage("emailの形式で入力してください。")
+    if(emptyMessage) setMessageErrorMessage("本文は必須です。")
+    if(insufficientMessageLength) setMessageErrorMessage("本文は500文字以内で入力してください。")
 
-    const enableSubmit =
-      !emptyName &&
-      !insufficientNameLength &&
-      !emptyEmail &&
-      regex.test(email) &&
-      !emptyMessage &&
-      !insufficientMessageLength;
+    const enableSubmit = !emptyName && !insufficientNameLength && !emptyEmail && regex.test(email) && !emptyMessage && !insufficientMessageLength
 
-    if (enableSubmit) {
-      const api_url =
-        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/contacts";
+
+    if(enableSubmit) {
+      const api_url = 'https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/contacts'
 
       fetch(api_url, {
         method: "post",
@@ -56,54 +49,47 @@ function Contact() {
           "Content-Type": "application/json",
         },
         // body: JSON.stringify(data)
-        body: JSON.stringify({ name, email, message }),
+        body: JSON.stringify(name, email, message)
       })
-        .then((response) => response.json())
-        .then((result) => alert("送信しました"))
-        .catch((error) => alert("正常に送信できませんでした"))
-        .then(() => {
-          setName("");
-          setEmail("");
-          setMessage("");
-          console.log(formData);
-        });
+      .then((response) => response.json())
+      .then((result) => alert("送信しました"))
+      .catch((error) => alert("正常に送信できませんでした"))
+      .then(() => {
+        setName("")
+        setEmail("")
+        setMessage("")
+        console.log(formData)
+      })
     }
-  };
+  }
 
-  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-  const onChangeMessage: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-    setMessage(e.target.value);
-  };
+  const onChangeName = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+  }
+  const onChangeEmail = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+  }
+  const onChangeMessage = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value)
+  }
 
-  const inputClear = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    e.preventDefault();
-    setName("");
-    setEmail("");
-    setMessage("");
-    setNameErrorMessage("");
-    setEmailErrorMessage("");
-    setMessageErrorMessage("");
-  };
+  const inputClear = (e:React.MouseEvent<HTMLElement, MouseEvent>) => {
+    e.preventDefault()
+    setName("")
+    setEmail("")
+    setMessage("")
+    setNameErrorMessage("")
+    setEmailErrorMessage("")
+    setMessageErrorMessage("")
+  }
 
-  return (
+  return(
     <div className="mx-auto mb-10 w-8/12 py-10">
-      <h1 className="text-xl text-black-500 font-bold mb-10">
-        問合わせフォーム
-      </h1>
+      <h1 className="text-xl text-black-500 font-bold mb-10">問合わせフォーム</h1>
       <form onSubmit={onSubmit}>
         <div className="md:flex items-center mb-6">
           <div className="md:w-1/4">
-            <label
-              className="block mb-1 md:mb-0 pr-4 text-black"
-              htmlFor="name"
-            >
-              お名前
-            </label>
+            <label className="block mb-1 md:mb-0 pr-4 text-black" htmlFor="name">お名前</label>
           </div>
           <div className="md:w-3/4">
             <input
@@ -114,19 +100,12 @@ function Contact() {
               value={name}
               onChange={onChangeName}
             />
-            {nameErrorMessage && (
-              <span className="text-red-600 text-sm">{nameErrorMessage}</span>
-            )}
+            {nameErrorMessage && <span className="text-red-600 text-sm">{nameErrorMessage}</span>}
           </div>
         </div>
         <div className="md:flex items-center mb-6">
           <div className="md:w-1/4">
-            <label
-              className="block mb-1 md:mb-0 pr-4 text-black"
-              htmlFor="email"
-            >
-              メールアドレス
-            </label>
+            <label className="block mb-1 md:mb-0 pr-4 text-black" htmlFor="email">メールアドレス</label>
           </div>
           <div className="md:w-3/4">
             <input
@@ -136,52 +115,33 @@ function Contact() {
               value={email}
               onChange={onChangeEmail}
             />
-            {emailErrorMessage && (
-              <span className="text-red-600 text-sm">{emailErrorMessage}</span>
-            )}
+            {emailErrorMessage && <span className="text-red-600 text-sm">{emailErrorMessage}</span>}
           </div>
         </div>
         <div className="md:flex items-center mb-6">
           <div className="md:w-1/4">
-            <label
-              className="block mb-1 md:mb-0 pr-4 text-black"
-              htmlFor="message"
-            >
-              本文
-            </label>
+            <label className="block mb-1 md:mb-0 pr-4 text-black" htmlFor="message">本文</label>
           </div>
           <div className="md:w-3/4">
             <textarea
               name="message"
               id="message"
-              cols={30}
-              rows={8}
+              cols="30"
+              rows="8"
               className="w-full p-4 border border-gray-300 rounded-lg"
               value={message}
               onChange={onChangeMessage}
             ></textarea>
-            {messageErrorMessage && (
-              <span className="text-red-600 text-sm">
-                {messageErrorMessage}
-              </span>
-            )}
+            {messageErrorMessage && <span className="text-red-600 text-sm">{messageErrorMessage}</span>}
           </div>
         </div>
         <div className="flex items-center justify-center">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded mx-2 rounded-lg">
-            送信
-          </button>
-          <button
-            className="font-bold bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded mx-2 rounded-lg"
-            onClick={inputClear}
-            type="button"
-          >
-            クリア
-          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded mx-2 rounded-lg">送信</button>
+          <button className="font-bold bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded mx-2 rounded-lg" onClick={inputClear} type="button">クリア</button>
         </div>
       </form>
     </div>
-  );
+  )
 }
 
-export default Contact;
+export default Contact
